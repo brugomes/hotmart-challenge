2182b9ccd7c8423192b31d01ac3a605d4e06f0ef
Bruna Gomes da Silva <brugomes@users.noreply.github.com>
2020-09-18 00:20:11 +1200

    Collapse creation to view expense

--- src/Components/timeline/timeline.js
+++ src/Components/timeline/timeline.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import './timeline.scss';
-import logo from './../../assets/img/status.png';
-import iconView from './../../assets/img/icon-view.jpg';
-import iconEdit from './../../assets/img/icon-edit.jpg';
-import iconDownload from './../../assets/img/icon-download.jpg';
-
 import TimelineItemDetail from './timelineItemDetail';
-
 
 function Timeline() {
     
@@ -33,38 +27,8 @@ function Timeline() {
         <div className="timeline-container">
             {   
                 timeline.map(item => item.expenseId !== undefined ?
-                    <div className="timeline-container__item" key={item.id} id={item.id}>
-                        <table>
-                            <tbody> 
-                                <tr>
-                                    <td>
-                                        <img src={require('./../../assets/img/icon-' + item.expenseTypeIcon + '.jpg')} alt="" />    
-                                        <span className="item-date">{new Intl.DateTimeFormat("en-GB").format(item.invoiceDate)}</span> 
-                                    </td>
-                                    <td>
-                                        <span className="item-title">TIPO</span>
-                                        <span className="item-detail">Aprovação da solicitação</span>
-                                        <span className="item-notes">{item.notes}</span>
-                                    </td>
-                                    <td>
-                                        <span className="item-title">VALOR</span>
-                                        <span className="item-detail">{item.currencyCode} {item.amountTotal}</span>
-                                        <span className="item-notes">Valor da nota: {item.currencyCode}{item.amountSpent}</span>
-                                    </td>
-                                    <td>
-                                        <span className="item-title">STATUS</span>
-                                        <div className={'item-status--' + item.status}><span>{item.status}</span></div>
-                                    </td>
-                                    <td>
-                                        <span className="item-options"><img src={iconView} alt=''/>Visualizar</span>
-                                        <span className="item-options"><img src={iconEdit} alt=''/>Editar</span>
-                                        <span className="item-options"><img src={iconDownload} alt=''/>Baixar</span>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table> 
-
-                        <TimelineItemDetail imgUrl={item.resourceUrl} />
+                    <div className="timeline-container__item" key={item.id} >
+                        <TimelineItemDetail item={item} />
                     </div>
                     : null
                 )
--- src/Components/timeline/timelineItemDetail.js
+++ src/Components/timeline/timelineItemDetail.js
@@ -1,15 +1,69 @@
 import React from "react";
-
 import './timelineItemDetail.scss';
 
-function TimelineItemDetail(props){
-    return (
-        <div>
-            <div className="img-detail">
-                <img src={props.imgUrl} alt='' />
+import iconView from './../../assets/img/icon-view.jpg';
+import iconEdit from './../../assets/img/icon-edit.jpg';
+import iconDownload from './../../assets/img/icon-download.jpg';
+
+
+class TimelineItemDetail extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+        this.collapse = this.collapse.bind(this);
+    }
+    collapse(e) {
+        this.setState({ open: !this.state.open })
+    }
+
+    componentDidUpdate() {
+    }
+
+    render() {
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <img src={require('./../../assets/img/icon-' + this.props.item.expenseTypeIcon + '.jpg')} alt="" />
+                                <span className="item-date">{new Intl.DateTimeFormat("en-GB").format(this.props.item.invoiceDate)}</span>
+                            </td>
+                            <td>
+                                <span className="item-title">TIPO</span>
+                                <span className="item-detail">Aprovação da solicitação</span>
+                                <span className="item-notes">{this.props.item.notes}</span>
+                            </td>
+                            <td>
+                                <span className="item-title">VALOR</span>
+                                <span className="item-detail">{this.props.item.currencyCode} {this.props.item.amountTotal}</span>
+                                <span className="item-notes">Valor da nota: {this.props.item.currencyCode}{this.props.item.amountSpent}</span>
+                            </td>
+                            <td>
+                                <span className="item-title">STATUS</span>
+                                <div className={'item-status--' + this.props.item.status}><span>{this.props.item.status}</span></div>
+                            </td>
+                            <td>
+                                <span className="item-options" onClick={(e) => this.collapse(e)}>
+                                    <img src={iconView} alt='' />
+                                    {this.state.open ? 'Fechar despesa' : 'Visualizar despesa'}</span>
+                                <span className="item-options"><img src={iconEdit} alt='' />Editar</span>
+                                <a href={this.props.item.resourceUrl} download className="item-options"><img src={iconDownload} alt='' />Baixar</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                {this.state.open ? (
+                    <div className="img-detail" >
+                        <img src={this.props.item.resourceUrl} alt='' />
+                    </div>
+                ) : null}
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default TimelineItemDetail;
